bda7f96409c8c1362ed5d751efa5b649c1f00da5
alexandre <alecarv98@gmail.com>
2024-11-20 16:33:34 -0300

    add validation and delegate to cart_item model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,12 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, numericality: { greater_than: 0, only_integer: true }
+
+  delegate :name, :price, to: :product, prefix: :product
+
+  def total_price
+    product_price * quantity
+  end
 end
