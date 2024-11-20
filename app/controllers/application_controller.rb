ff1d54184a5d940b28da35c514b4e7264404eb2c
alexandre <alecarv98@gmail.com>
2024-11-20 15:18:58 -0300

    add current_cart in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  before_action :current_cart
+
+  private
+
+  def current_cart
+    cart = Cart.last
+    if cart
+      @current_cart = cart
+    else
+      @current_cart = Cart.create!(total_price: 0)
+    end
+  end
 end
