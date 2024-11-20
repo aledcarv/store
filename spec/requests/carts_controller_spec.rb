5ba7c7024d4ece07cb08cfb9f7518328df8aa841
alexandre <alecarv98@gmail.com>
2024-11-20 20:02:30 -0300

    add request specs for update request

--- spec/requests/carts_controller_spec.rb
+++ spec/requests/carts_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe CartsController, type: :request do
-  pending "TODO: Escreva os testes de comportamento do controller de carrinho necessários para cobrir a sua implmentação #{__FILE__}"
   describe "POST /add_items" do
     let(:cart) { Cart.create }
     let(:product) { Product.create(name: "Test Product", price: 10.0) }
@@ -114,6 +113,103 @@ RSpec.describe CartsController, type: :request do
               "id" => Cart.last.id,
               "products" => [],
               "total_price" => Cart.last.total_price
+            }
+          )
+        end
+      end
+    end
+  end
+
+  describe 'PUT /cart' do
+    context 'when the product is not in the cart' do
+      let(:product) { create(:product) }
+
+      it 'add product in the cart' do
+        put '/cart', params: { product_id: product.id, quantity: 1 }
+
+        expect(response).to be_successful
+        expect(JSON.parse(response.body)).to eq(
+          {
+            "id" => Cart.last.id,
+            "products" => [
+              {
+                "id" => product.id,
+                "name" => CartItem.last.product_name,
+                "quantity" => CartItem.last.quantity,
+                "unit_price" => CartItem.last.product_price,
+                "total_price" => CartItem.last.total_price
+              }
+            ],
+            "total_price" => Cart.last.total_price
+          }
+        )
+      end
+
+      context 'and quantity is not provided' do
+        it 'show message error' do
+          put '/cart', params: { product_id: product.id }
+
+          expect(response).to have_http_status(:bad_request)
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "error" => "Validation failed: Quantity can't be blank, Quantity is not a number"
+            }
+          )
+        end
+      end
+
+      context 'and quantity is zero' do
+        it 'show message error' do
+          put '/cart', params: { product_id: product.id, quantity: 0 }
+
+          expect(response).to have_http_status(:bad_request)
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "error" => "Validation failed: Quantity must be greater than 0"
+            }
+          )
+        end
+      end
+
+      context 'and product_id is not provided' do
+        it 'show message error' do
+          put '/cart', params: { quantity: 1 }
+
+          expect(response).to have_http_status(:bad_request)
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "error" => "Validation failed: Product must exist"
+            }
+          )
+        end
+      end
+
+      context 'and product_id does not exist' do
+        it 'show message error' do
+          put '/cart', params: { product_id: 548, quantity: 1 }
+
+          expect(response).to have_http_status(:bad_request)
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "error" => "Validation failed: Product must exist"
+            }
+          )
+        end
+      end
+
+      context 'and try to add product already in the cart' do
+        let(:cart) { create(:cart) }
+        let(:cart_item) { create(:cart_item, product: product, cart: cart) }
+
+        before { cart_item }
+
+        it 'show message error' do
+          put '/cart', params: { product_id: product.id, quantity: 1 }
+
+          expect(response).to have_http_status(:conflict)
+          expect(JSON.parse(response.body)).to eq(
+            {
+              "error" => "product already in the cart"
             }
           )
         end
