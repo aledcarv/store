9a5210562d8afcb83f66da7db9a3d4952155e97a
alexandre <alecarv98@gmail.com>
2024-11-20 15:19:41 -0300

    change code in carts_controller#show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,9 @@
 class CartsController < ApplicationController
-  before_action :set_cart
-
   def show
     render json: {
-      id: @cart.id,
-      products: @cart.products.as_json(except: [:created_at, :updated_at])
+      id: @current_cart.id,
+      products: @current_cart.products.as_json(except: [:created_at, :updated_at])
       .map { |p| p.merge('price' => p['price'].to_f) }
     }
-  end
-
-  private
-
-  def set_cart
-    @cart = Cart.last
   end
 end
