e9e61c17559435ccc6d5f44c9d7f5c7a40ca0c98
alexandre <alecarv98@gmail.com>
2024-11-20 20:49:28 -0300

    add method calculate_total_price in cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,8 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items
 
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
+
+  def calculate_total_price
+    cart_items.sum(&:total_price)
+  end
 end
