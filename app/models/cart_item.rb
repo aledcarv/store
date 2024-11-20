51ab9603fdb87bc4f570d1748628295a5e727039
alexandre <alecarv98@gmail.com>
2024-11-20 20:01:50 -0300

    add presence validation for quantity in cart_item

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
+  validates  :quantity, presence: true
   validates :quantity, numericality: { greater_than: 0, only_integer: true }
 
   delegate :name, :price, to: :product, prefix: :product
