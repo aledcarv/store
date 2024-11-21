02376083d9658afae82403ed78589504e9ad4c02
alexandre <alecarv98@gmail.com>
2024-11-21 00:08:32 -0300

    add route to delete product from cart

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
   resource :cart, only:  %i[show update] do
+    delete '/:product_id', to: 'carts#destroy'
     resource :add_item, only: %i[update]
   end
   get "up" => "rails/health#show", as: :rails_health_check
