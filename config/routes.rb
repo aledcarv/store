a6a06e597197c249322f8cdbf544b57936ba8645
alexandre <alecarv98@gmail.com>
2024-11-20 23:15:52 -0300

    add new route add_item

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
-  resource :cart, only:  %i[show update]
+  resource :cart, only:  %i[show update] do
+    resource :add_item, only: %i[update]
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
