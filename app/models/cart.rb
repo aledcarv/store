f533aeb78505700e56baf0ae74cbd1866c4d7dbf
alexandre <alecarv98@gmail.com>
2024-11-21 19:49:55 -0300

    add scope

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,9 @@ class Cart < ApplicationRecord
   has_many :cart_items
   has_many :products, through: :cart_items
 
+  scope :all_abandoned, -> { where('updated_at < ?', 3.hours.ago).active }
+  scope :old_abandoned, -> { where('updated_at < ?', 7.days.ago).abandoned }
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   enum :status, [:active, :abandoned], default: :active
