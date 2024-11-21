f817e68c359f0c2b2f1693cd4f391860bdb2efc1
alexandre <alecarv98@gmail.com>
2024-11-21 09:08:05 -0300

    add column status in cart table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_20_152611) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_21_115610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_20_152611) do
     t.decimal "total_price", precision: 17, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", null: false
   end
 
   create_table "products", force: :cascade do |t|
