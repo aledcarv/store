2cdc1d82bbf5da3b832c9d909ff65f1915a1ddfe
alexandre <alecarv98@gmail.com>
2024-11-20 23:16:39 -0300

    remove unecessary test

--- spec/requests/carts_controller_spec.rb
+++ spec/requests/carts_controller_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe CartsController, type: :request do
-  describe "POST /add_items" do
-    let(:cart) { Cart.create }
-    let(:product) { Product.create(name: "Test Product", price: 10.0) }
-    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }
-
-    context 'when the product already is in the cart' do
-      subject do
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-      end
-
-      it 'updates the quantity of the existing item in the cart' do
-        expect { subject }.to change { cart_item.reload.quantity }.by(2)
-      end
-    end
-  end
-
   describe "GET /cart" do
     context 'when the cart is empty' do
       let(:cart) { create(:cart) }
