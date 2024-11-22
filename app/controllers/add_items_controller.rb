5c601924cb929c575a7d86b5a4e18d9cd903d565
alexandre <alecarv98@gmail.com>
2024-11-21 22:01:28 -0300

    add small refactoring in add_items_controller

--- app/controllers/add_items_controller.rb
+++ app/controllers/add_items_controller.rb
@@ -1,24 +1,17 @@
 class AddItemsController < ApplicationController
-  # TODO: move code to another class and add exceptions
   def update
-    if params[:quantity].to_i.zero?
-      render json: { error: "Quantity can't be blank" }, status: :bad_request
-    elsif params[:product_id].to_i.zero?
-      render json: { error: 'Product must exist' }, status: :bad_request
-    else
-      cart_item = @current_cart.cart_items.find_by(product_id: params[:product_id])
+    IncreaseCartItemQuantity.new(**send_args).call
 
-      if cart_item
-        current_quantity = cart_item.quantity
-        quantity = current_quantity + params[:quantity].to_i
+    render json: CartSerializer.new(@current_cart).as_json, status: :ok
+  rescue IncreaseCartItemQuantity::ErrorHandler => e
+    render json: { error: e.message }, status: :bad_request
+  end
 
-        cart_item.update!(quantity: quantity)
-        @current_cart.update!(total_price: @current_cart.calculate_total_price)
+  private
 
-        render json: CartSerializer.new(@current_cart).as_json, status: :ok
-      else
-        render json: { error: 'Product must exist' }, status: :bad_request
-      end
-    end
+  def send_args
+    { product_id: params[:product_id].to_i,
+      quantity: params[:quantity].to_i,
+      cart: @current_cart }
   end
 end
